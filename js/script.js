6e5573bf64b5fa38497b0a128c61aae9d6c0dc4e
youplayer7 <93318522+youplayer7@users.noreply.github.com>
2025-03-15 19:56:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,12 +14,12 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
-var allCategoriesUrl = 
-  "https://davids-restaurant.herokuapp.com/categories.json";
+var allCategoriesUrl =
+  "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl = 
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsUrl =
+  "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
